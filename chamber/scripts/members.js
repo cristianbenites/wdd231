0dfb35d76afa0a658ce912ccc06e49282548dcc4
cristianbenites <cristian.jbenites@gmail.com>
2025-03-15 21:01:10 -0400

    fix creation of member cards

--- chamber/scripts/members.js
+++ chamber/scripts/members.js
@@ -4,10 +4,9 @@ const buildData = async () => {
     const response = await fetch(url);
     const { members } = await response.json();
 
-    const section = document.getElementById('members');
-
     const cards = members.map(createMemberCard);
 
+    document.getElementById('members').replaceChildren(...cards);
 }
 
 const createMemberCard = (member) => {
@@ -21,27 +20,42 @@ const createMemberCard = (member) => {
     tagline.classList.add('tagline');
     tagline.textContent = member.address;
 
-                    <div class="member-card">
-                        <h2>Business Name</h2>
-                        <p class="tagline">Business Tag Line</p>
-                        <hr>
-                        <img src="" alt="">
-                        <p class="email">EMAIL: <span>info@example.com</span></p>
-                        <p class="phone">PHONE: <span>+55 11 3000-0000</span></p>
-                        <p class="url">URL: <span>example.com.br</span></p>
-                    </div>
+    const hr = document.createElement('hr');
 
-    "name": "PeakClimb Gear",
-    "address": "Rocade des Grimpeurs, Timbuktu, Mali",
-    "phone_number": "+1 555-0707",
-    "website_url": "https://www.peakclimbgear.com",
-    "image_file": "peakclimb_icon.png",
-    "membership_level": 2,
-    "industry": "Outdoor Equipment",
-    "featured_products": ["Hiking Boots", "Climbing Harnesses", "Tents"]
+    const img = document.createElement('img');
+    img.src = member.image_file;
+    img.alt = `${member.name} Logo`;
+
+    const email = document.createElement('p');
+    email.classList.add('email');
+    email.textContent = 'EMAIL: ';
+    const emailSpan = document.createElement('span');
+    emailSpan.textContent = member.email;
+    email.appendChild(emailSpan);
+
+    const phone = document.createElement('p');
+    phone.classList.add('phone');
+    phone.textContent = 'PHONE: ';
+    const phoneSpan = document.createElement('span');
+    phoneSpan.textContent = member.phone_number;
+    phone.appendChild(phoneSpan);
+
+    const url = document.createElement('p');
+    url.classList.add('url');
+    url.textContent = 'URL: ';
+    const urlSpan = document.createElement('span');
+    urlSpan.textContent = member.website_url;
+    url.appendChild(urlSpan);
+
+    card.appendChild(h2);
+    card.appendChild(tagline);
+    card.appendChild(hr);
+    card.appendChild(img);
+    card.appendChild(email);
+    card.appendChild(phone);
+    card.appendChild(url);
 
     return member;
-
 };
 
 
