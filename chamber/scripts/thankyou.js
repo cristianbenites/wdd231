62c6be5743018ebb74648493de13f7807167a57c
cristianbenites <cristian.jbenites@gmail.com>
2025-03-28 21:52:13 -0400

    fix audit tool problems

--- chamber/scripts/thankyou.js
+++ chamber/scripts/thankyou.js
@@ -6,6 +6,7 @@ function buildThankYou() {
     document.getElementById('org').textContent = params.get('organization');
     document.getElementById('email').textContent = params.get('email');
     document.getElementById('phone').textContent = params.get('phone');
+    document.getElementById('business').textContent = params.get('business');
     document.getElementById('memlvl').textContent = params.get('membership-level');
     document.getElementById('timestamp').textContent = params.get('timestamp');
 }
